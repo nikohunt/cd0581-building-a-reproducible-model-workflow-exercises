8c931e20ba8f50873e50bea7b2a0f1eca43d4a99
Nikolas Hunt <nikolas.hunt@dyson.com>
2022-09-17 15:04:31 +0100

    Add mlflow run for process_data

--- lesson-1/exercises/exercise_3/starter/main.py
+++ lesson-1/exercises/exercise_3/starter/main.py
@@ -1,11 +1,12 @@
-import mlflow
 import os
+
 import hydra
+import mlflow
 from omegaconf import DictConfig
 
 
 # This automatically reads in the configuration
-@hydra.main(config_name='config')
+@hydra.main(config_name="config")
 def go(config: DictConfig):
 
     # Setup the wandb experiment. All runs will be grouped under this name
@@ -22,17 +23,19 @@ def go(config: DictConfig):
             "file_url": config["data"]["file_url"],
             "artifact_name": "iris.csv",
             "artifact_type": "raw_data",
-            "artifact_description": "Input data"
+            "artifact_description": "Input data",
         },
     )
-
-    ##################
-    # Your code here: use the artifact we created in the previous step as input for the `process_data` step
-    # and produce a new artifact called "cleaned_data".
-    # NOTE: use os.path.join(root_path, "process_data") to get the path
-    # to the "process_data" component
-    ##################
-
+    _ = mlflow.run(
+        os.path.join(root_path, "process_data"),
+        "main",
+        parameters={
+            "input_artifact": config["data"]["input_artifact"],
+            "artifact_name": "cleaned_data",
+            "artifact_type": "cleaned_data",
+            "artifact_description": "Raw data for processing",
+        },
+    )
 
 
 if __name__ == "__main__":
